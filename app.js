a2f785811507d1f7eeb69838f3b965844e031a2f
rodolfoconci <r.conci@hotmail.com>
2024-10-17 20:12:35 -0300

    endpoint de test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import  cors from "cors";
 import usersRouter from "./routes/users.js";
 import polizasRouter from "./routes/polizas.js";
+import testRouter from "./routes/test.js";
 
 const PORT = process.env.PORT;
 const app = express();
@@ -10,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/polizas", polizasRouter);
+app.use("/api/test", testRouter);
 
 app.listen(PORT, () => {
   console.log("Servidor Web en el puerto:", PORT);
 });
+
