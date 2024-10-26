93b54891da200709a7b831624488cc17cde0c5c0
lukitasmois <lmoix85@gmail.com>
2024-10-26 12:40:13 -0300

    Correccion de errores

--- routes/polizas.js
+++ routes/polizas.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addPoliza, getPolizas, getPolizasAsegurado } from "../data/poliza.js";
+import { addPoliza, getPolizas,} from "../data/poliza.js";
 import auth from "../middleware/auth.js";
 
 const polizasRouter = express.Router();
