53e695b77cc8bdb7f24c713f49fc1263e8e03513
lukitasmois <lmoix85@gmail.com>
2024-11-02 14:05:49 -0300

    Agrego mensaje de error en la validacion de token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken";
 
+const MSG_ERROR_PERMISOS = "No tiene permisos para realizar esta acción.";
+
 async function auth(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json({ error: MSG_ERROR_PERMISOS });
 
   jwt.verify(token, process.env.CLAVE_SECRETA, (err, user) => {
-      if (err) return res.sendStatus(403);
+      if (err) return res.status(403).json({ error: MSG_ERROR_PERMISOS });
       req.user = user;
       next();
   });
